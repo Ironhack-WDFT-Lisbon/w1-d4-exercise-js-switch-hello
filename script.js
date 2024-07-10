c5b5dd1efabd9bda153f9e36ac9615715b848b09
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-07-10 13:09:23 +0100

    update exercise jul 24

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Ready to go! 🚀");
 
-// const language = prompt(
-//   "In which language do you want to see the message? (Spanish, French, Italian, Portuguese, German, Dutch, Maori English)"
-// );
+const language = prompt(
+  "In which language do you want to see the message? (Spanish, French, Italian, Portuguese, German, Dutch, Maori English)"
+);
 
 // ------------------------------
 // If ... else code
@@ -20,28 +20,3 @@ console.log("Ready to go! 🚀");
 // ------------------------------
 // Refactor to Switch
 // ------------------------------
-// switch (language) {
-//   case "Spanish":
-//     console.log("Hola, mundo!");
-//     break;
-//   case "French":
-//     console.log("Bonjour, le monde!");
-//     break;
-//   case "Italian":
-//     console.log("Ciao, mondo!");
-//     break;
-//   case "Portuguese":
-//     console.log("Olá, mundo!");
-//     break;
-//   case "German":
-//     console.log("Hallo, Welt!");
-//     break;
-//   case "Dutch":
-//     console.log("Hallo, wereld!");
-//     break;
-//   case "Maori":
-//     console.log("Kia ora, ao!");
-//     break;
-//   default:
-//     console.log("Hello, world!");
-// }
